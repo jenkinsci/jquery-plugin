faf6402fd962882e785845e0e3c127057713ef5c
kiy0taka <kiy0taka333+github@gmail.com>
2011-02-03 01:47:14 +0900

    change ordinal.

--- src/main/java/hudson/plugins/jquery/JQuery.java
+++ src/main/java/hudson/plugins/jquery/JQuery.java
@@ -3,7 +3,7 @@ package hudson.plugins.jquery;
 import hudson.Extension;
 import hudson.model.PageDecorator;
 
-@Extension(ordinal=1)
+@Extension(ordinal=10)
 public class JQuery extends PageDecorator {
 
     public JQuery() {
